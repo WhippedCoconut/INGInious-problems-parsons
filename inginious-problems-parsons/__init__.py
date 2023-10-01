ba5997fb0fae19bbf97b1741bf1352d856c26e12
WhippedCoconut <corentin.lengele@student.uclouvain.be>
2023-10-01 19:34:28 +0200

    Added dynamic content on task page

--- inginious-problems-parsons/__init__.py
+++ inginious-problems-parsons/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import os
+import sys
 
 from flask import send_from_directory
 
@@ -26,7 +27,23 @@ class ParsonsProblem(Problem):
     def __init__(self, problemid, content, translations, taskfs):
         Problem.__init__(self, problemid, content, translations, taskfs)
         self._header = content['header'] if "header" in content else ""
-        self._answer = str(content.get("answer", ""))
+        self._shuffle = True if 'shuffle' in content else False
+        self._choices = []
+        if "choices" not in content or not isinstance(content['choices'], (list, tuple)):
+            raise Exception(problemid + " does not have choices or choices are not an array")
+        for index, choice in enumerate(content["choices"]):
+            data = {"index": index}
+            if 'content' not in choice:
+                raise Exception("A choice in " + problemid + " does not have content")
+            data['content'] = choice['content']
+            if 'conditions' not in choice:
+                raise Exception("A choice in " + problemid + " does not have conditions")
+            data['conditions'] = choice['conditions']
+            if 'distractor' in choice:
+                data['distractor'] = True
+            if 'nested' in choice:
+                data['nested'] = True
+            self._choices.append(data)
 
     @classmethod
     def get_type(cls):
@@ -43,7 +60,19 @@ class ParsonsProblem(Problem):
 
     @classmethod
     def parse_problem(self, problem_content):
-        return Problem.parse_problem(problem_content)
+        problem_content = Problem.parse_problem(problem_content)
+        if "shuffle" in problem_content:
+            problem_content["distractor"] = True
+
+        if "choices" in problem_content:
+            problem_content["choices"] = [val for _, val in
+                                          sorted(iter(problem_content["choices"].items()), key=lambda x: int(x[0]))]
+            for choice in problem_content["choices"]:
+                if "distractor" in choice:
+                    choice["distractor"] = True
+                if "nested" in choice:
+                    choice["nested"] = True
+        return problem_content
 
     @classmethod
     def get_text_fields(cls):
@@ -61,8 +90,8 @@ class DisplayableParsonsProblem(ParsonsProblem, DisplayableProblem):
         """ Show ParsonsProblem """
         header = ParsableText(self.gettext(language, self._header), "rst",
                               translation=self.get_translation_obj(language))
-        return template_helper.render("parsons.html",
-                                      template_folder=PATH_TO_TEMPLATES, inputId=self.get_id(), header=header)
+        return template_helper.render("parsons.html", template_folder=PATH_TO_TEMPLATES,
+                                      inputId=self.get_id(), header=header, choices=self._choices)
 
     @classmethod
     def show_editbox(cls, template_helper, key, language):
@@ -76,5 +105,5 @@ class DisplayableParsonsProblem(ParsonsProblem, DisplayableProblem):
 def init(plugin_manager, course_factory, client, plugin_config):
     plugin_manager.add_page('/plugins/parsons/static/<path:path>', StaticMockPage.as_view("parsonsproblemstaticpage"))
     plugin_manager.add_hook("javascript_footer", lambda: "/plugins/parsons/static/lib/Sortable.min.js")
-    plugin_manager.add_hook("javascript_header", lambda: "/plugins/parsons/static/parsons.js")
+    plugin_manager.add_hook("javascript_footer", lambda: "/plugins/parsons/static/parsons.js")
     course_factory.get_task_factory().add_problem_type(DisplayableParsonsProblem)
--- inginious-problems-parsons/static/parsons.js
+++ inginious-problems-parsons/static/parsons.js
@@ -1,3 +1,19 @@
+function studio_init_template_parsons(well, pid, problem) {
+    jQuery.each(problem["choices"], function(index, elem) {
+        elem.index = index;
+        parsons_create_choice(pid, elem);
+    });
+}
+
+function load_input_parsons(submissionid, key, input) {
+    console.log(key);
+    console.log(input);
+}
+
+function load_feedback_parsons(key, content) {
+    load_feedback_code(key, content);
+}
+
 
 function parsons_create_choice(pid, choice_data){
     var well = $(studio_get_problem(pid));
@@ -9,16 +25,23 @@ function parsons_create_choice(pid, choice_data){
     var row = $("#subproblem_parsons_choice").html();
     var new_row_content = row.replace(/PID/g, pid).replace(/CHOICE/g, index);
     var new_row = $("<div></div>").attr('id', 'choice-' + index + '-' + pid).html(new_row_content);
-    console.log(new_row);
     $("#choices-" + pid, well).append(new_row);
 
-    var editor = registerCodeEditor($(".parsons_conditions", new_row)[0], 'rst', 1);
+    if("content" in choice_data){
+        $(".parsons_content_" + choice_data["index"]).val(choice_data["content"]);
+    }
 
+    var editor = registerCodeEditor($(".parsons_conditions_" + choice_data["index"], new_row)[0], 'rst', 1);
     if("conditions" in choice_data)
         editor.setValue(choice_data["conditions"]);
 
-    if("distractor" in choice_data && choice_data["distractor"] == true) {
-        var checkbox = $(".parsons_distractor");
+    if("distractor" in choice_data && choice_data["distractor"] === true) {
+        var checkbox = $(".parsons_distractor_" + choice_data["index"]);
+        checkbox.click();
+    }
+
+    if("nested" in choice_data && choice_data["nested"] === true) {
+        var checkbox = $(".parsons_nested_" + choice_data["index"]);
         checkbox.click();
     }
 }
@@ -30,8 +53,6 @@ function parsons_delete_choice(pid, choice) {
 function init_task_drag_and_drop() {
     var choices = document.getElementById("task-choices");
     var	solution = document.getElementById("task-solution");
-    console.log(choices);
-    console.log(solution);
 
     new Sortable(choices, {
         group: 'sharable',
